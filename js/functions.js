3586604eb66705d275f724e9d355610f686cc470
olsen-andrew <ols09028@byui.edu>
2017-05-17 00:55:06 -0500

    Start of Galleries App (sandbox)

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,19 @@
-console.log('connected');
+//object constructor for galleries
+function gallery(name){ 
+	this.title = name;
+	this.numSlides = function(){
+
+	};
+	alert(name);
+} 
+
+var thisGallery = function(){
+	var galName = this.getAttribute("name");
+	gallery(galName);
+}
+
+var galleries = document.getElementsByClassName('gallery');
+console.log(galleries.length);
+for (var i = 0; i < galleries.length; i++){
+	galleries[i].addEventListener('click',thisGallery,false);
+}
