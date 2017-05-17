cb465f790986112fb390c2011a6ccc6fd0d96031
olsen-andrew <ols09028@byui.edu>
2017-05-17 03:00:22 -0500

    done for the night

--- js/galleries.js
+++ js/galleries.js
@@ -1,24 +1,18 @@
-function addGalToPage(){
 
-}
 
 //object constructor for galleries
 var galleries = [];
 function Gallery(name, images){ 
-	var htmlEl = "<div class='gallery'></div>";
-
 	this.title = name;
 	this.images = images;
 	this.numSlides = function(){
 
 	};
-	alert(name);
-	addGalToPage();
 } 
 
 //check for browser support
 if(document.getElementsByClassName){
-	var getGalleries = document.getElementsByClassName('gallery');
+	var getGalleries = document.getElementsByClassName('galleryThm');
 } else{
 	alert("Your Browser is Out of Date.");
 }
@@ -26,11 +20,23 @@ if(document.getElementsByClassName){
 // onClick -> this function
 var thisGallery = function(){
 	var images = JSON.parse(this.getAttribute("data-images"));
-	
-	console.log(images.files);
+	console.log(images.files.length);
 	var galName = this.getAttribute("name");
 	var g = new Gallery(galName);
 	galleries.push(g);
+
+	//add gallery to page
+	var body = document.getElementsByTagName('body')[0],
+        div = document.createElement('div');
+    div.id = galName;
+    div.className = 'gallery';
+    body.appendChild(div);
+    for (var i = 0; i < images.files.length;i++){
+    	var div = document.getElementById(galName),
+    		img = document.createElement('img');
+    	img.src = 'galleries/'+images.files[i];
+    	div.appendChild(img);
+    }
 }
 
 // add event listeners for each gallery thumb
